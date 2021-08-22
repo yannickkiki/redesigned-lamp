c492e8e034731e6f1cc48829404e99f0273d678b
Yannick KIKI <seyive.kiki@gmail.com>
2021-08-22 16:34:22 +0100

    Add post example

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
+    return {"message": "Here we are"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
